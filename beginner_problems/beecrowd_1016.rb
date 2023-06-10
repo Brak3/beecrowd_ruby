364d1730646af14616d047ed2a6802262cd7201f
Brak3 <gabrielvieira040@outlook.com>
2023-06-10 17:45:17 -0300

    Resoluçõe

---
+++ beginner_problems/beecrowd_1016.rb
@@ -0,0 +1,7 @@
+
+km = gets.chomp.to_i
+
+min = km * 2
+
+puts "#{min} minutos"
+
---
+++ beginner_problems/beecrowd_1017.rb
@@ -0,0 +1,4 @@
+spent_time = gets.chomp.to_i
+average_speed = gets.chomp.to_i
+
+consumo = (average_speed * spent_time)/12
